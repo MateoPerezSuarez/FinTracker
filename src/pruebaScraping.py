1b44c2585ba723b0d88c347e378a184ab28137d4
MateoPerezSuarez <mateo.perez.suarez@opendeusto.es>
2025-10-08 15:13:11 +0200

    na

---
+++ src/pruebaScraping.py
@@ -0,0 +1,19 @@
+import asyncio, aiohttp, async_timeout, random, re, time
+from aiohttp import ClientTimeout
+from numpy import dtypes
+import pandas as pd
+import trafilatura
+from trafilatura.settings import use_config
+from newspaper import Article
+import tldextract
+from tqdm import tqdm
+
+import pandas as pd, csv
+
+
+
+
+
+
+
+
